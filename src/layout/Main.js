473925c6aa39ffdc16a1861e6bf6675877d52a52
Viktor080181 <lozhnvikt0801@mail.ru>
2025-09-10 22:25:09 +0300

    Initial commit

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,48 +1,65 @@
 import React from 'react';
 import Preloader from '../components/Preloader.js';
-import MovieList from '../components/MovieList.js';
+import MusicList from '../components/MusicList.js';  // Новый компонент вместо MovieList
 import Search from '../components/Search.js';
 import './Main.css';
 
-class Main extends React.Component
-{
-    state = { movies:[], loading:false, type:"all" }
-    componentDidMount()
-    {
-        fetch('https://omdbapi.com/?apikey=94dbc433&s=Matrix')
-        .then(response => response.json())
-        .then(data => this.setState({movies:data.Search}))
+class Main extends React.Component {
+    state = { tracks: [], loading: false, type: "all" };
+
+    componentDidMount() {
+        // Начальный поиск: треки по "Queen"
+        fetch('https://ws.audioscrobbler.com/2.0/?method=track.search&track=Queen&api_key=b4cdbc073401accd6cbd9c94796ca890&format=json')
+            .then(response => response.json())
+            .then(data => {
+                if (data.results && data.results.trackmatches && data.results.trackmatches.track) {
+                    this.setState({ tracks: data.results.trackmatches.track });
+                }
+            })
+            .catch(error => console.error('Error fetching initial tracks:', error));
     }
-    searchMovie = (str, type = 'all') =>
-    {
-        this.setState({loading:true})
-        fetch(`https://omdbapi.com/?apikey=94dbc433&s=${str.trim()}${type !== 'all' ? `&type=${type}` : ''}`)
-        .then(response => response.json())
-        .then
-        (
-            data => 
-            {
-                if(data.Response === "True")this.setState({movies: data.Search, loading:false});
-                else this.setState({movies:[], loading:false});
-            }
-        )
+
+    searchMusic = (str, type = 'all') => {
+        this.setState({ loading: true });
+        let url = `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${str.trim()}&api_key=b4cdbc073401accd6cbd9c94796ca890&format=json`;
         
-            // this.setState({loading:false});
-    }
-    render()
-    {
-        return(
+        // Если type !== 'all', можно добавить фильтр (например, для артистов)
+        if (type === 'artist') {
+            url = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${str.trim()}&api_key=b4cdbc073401accd6cbd9c94796ca890&format=json`;
+        }
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (data.results && data.results.trackmatches && data.results.trackmatches.track) {
+                    this.setState({ tracks: data.results.trackmatches.track, loading: false });
+                } else if (data.results && data.results.artistmatches && data.results.artistmatches.artist) {
+                    // Для артистов: адаптируй под MusicList
+                    this.setState({ tracks: data.results.artistmatches.artist, loading: false });
+                } else {
+                    this.setState({ tracks: [], loading: false });
+                }
+            })
+            .catch(error => {
+                console.error('Error searching music:', error);
+                this.setState({ loading: false });
+            });
+    };
+
+    render() {
+        return (
             <div className='main'>
                 <div className='wrap'>
-                    <Search searchMovie={this.searchMovie} />
+                    <Search searchMusic={this.searchMusic} />  {/* Передаём функцию поиска */}
                     {
-                        !this.state.loading && this.state.movies.length ? <MovieList movies={this.state.movies} /> : <Preloader />
-                        // if(!this.state.loading)
-                        //     this.state.movies.length ? <MovieList movies={this.state.movies} /> : <Preloader />
+                        !this.state.loading && this.state.tracks.length ? 
+                            <MusicList tracks={this.state.tracks} /> : 
+                            <Preloader />
                     }
                 </div>
             </div>
-        )
+        );
     }
 }
+
 export default Main;
